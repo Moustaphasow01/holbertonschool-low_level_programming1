d12bec2c2bb2661d8ec2a66c9639d4e15de93b67
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-06 13:03:14 -0700

    fix betty\'s bug

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- */
-int main (void)
+*main-prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+*return: 0
+*/
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+  puts ("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
