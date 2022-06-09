20155f8112da9bd9ec5cbd341b4b6b16105078c0
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-09 04:07:27 -0700

    fix bettu bug

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet - prints alphabet 10 times, in lowercase,
+* print_alphabet_x10 - prints alphabet 10 times, in lowercase,
 * followed by a new line
 */
 void print_alphabet_x10(void)
 {
 char c;
-int i=0;
-while (i<10)
+int i = 0;
+while (i < 10)
 {
 c = 'a';
 while (c <= 'z')
