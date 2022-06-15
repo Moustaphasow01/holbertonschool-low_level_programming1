68a550bdbac201fe5a33f6fc431101c476a70836
Moustaphasow01 <105611478+Moustaphasow01@users.noreply.github.com>
2022-06-15 11:54:37 +0200

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,18 @@
 /**
 * puts2 - prints one char out of 2 of a string
 * followed by a new line
-* @str: string
+* @str: string to print the chars from
 */
 void puts2(char *str)
 {
 int z, i;
 
 z = 0;
+
 while (str[len] != '\0')
 {
 z++;
 }
-
 for (i = 0; i < z; i += 2)
 {
 _putchar(str[i]);
