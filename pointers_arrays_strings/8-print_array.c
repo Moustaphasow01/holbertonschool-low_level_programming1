f5784c23b2cd3e9e8b3f9662e99f63d66a1eeaf7
Moustaphasow01 <105611478+Moustaphasow01@users.noreply.github.com>
2022-06-16 05:51:21 +0200

    Update 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,8 +4,8 @@
 /**
 * print_array - prints n elements of an array of integers
 * followed by a new line
-* @a: array 
-* @n: number of elements 
+* @a: array
+* @n: number of elements
 */
 void print_array(int *a, int n)
 {
