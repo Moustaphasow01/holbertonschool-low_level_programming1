71df006f56b2311ea95af096ee7a9fc226f77cf2
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-15 20:46:21 -0700

    update 9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
-#tring pointed to by src
+/** 
+* string pointed to by src
 * including the terminating null byte (\0)
 * to the buffer pointed to by dest
 * @dest: pointer to the buffer in which we copy the string
