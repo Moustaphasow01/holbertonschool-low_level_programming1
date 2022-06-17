5e7163ff875f6fd24e62d8ec7c56df91757723d8
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-17 06:15:46 -0700

    update main.j with protypes for the more pointers and more string project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,21 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 /*function to put out */
 int _putchar(char c);
+/*Prtotypes for more pointers,array and strings */
+/*function that concatenates two strings.*/
+char *_strcat(char *dest, char *src);
+/* function that concatenates two strings.*/
+char *_strncat(char *dest, char *src, int n);
+/* function that copies a string.*/
+char *_strncpy(char *dest, char *src, int n);
+/*function that compares two strings.*/
+int _strcmp(char *s1, char *s2);
+/*function that reverses the content of an array of integers.*/
+void reverse_array(int *a, int n);
+/*function that changes all lowercase letters of a string to uppercase.*/
+char *string_toupper(char *);
+/*function that capitalizes all words of a string.*/
+char *cap_string(char *);
+/*function that encodes a string into 1337.*/
+char *leet(char *);
 #endif
