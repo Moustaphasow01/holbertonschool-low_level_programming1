bd0e63c0e3cfdddf1d91d8d7e8154044e28072f1
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-20 04:13:45 -0700

    function that returns the factorial of a given number.

--- recursion/main.h
+++ recursion/main.h
@@ -11,4 +11,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 /*function that returns the length of a string.*/
 int _strlen_recursion(char *s);
+/* function that returns the factorial of a given number.*/
+int factorial(int n);
 #endif
