8a457141c55f2834f240102ac6fdc256b2f912e1
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-20 03:51:48 -0700

    adding the putchar function

---
+++ recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+/**
+*This is the the file with all the function s prototype for the recursion project 
+*/
+/*function to put out */
+int _putchar(char c);
+#endif
