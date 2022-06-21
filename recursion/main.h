cddab442b33c18d1f7c90b42240661baddde7304
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-06-21 05:31:55 -0700

    6-is_prime_number.c

--- recursion/main.h
+++ recursion/main.h
@@ -18,4 +18,7 @@ int _pow_recursion(int x, int y);
 /*function that returns the natural square root of a number.*/
 int _sqrt_recursion(int n);
 int actual_sqrt_recursion(int n, int i);
+/* function that returns 1 if the input integer is a prime number, otherwise return 0.*/
+int is_prime_number(int n);
+int actual_prime(int n, int i);
 #endif
